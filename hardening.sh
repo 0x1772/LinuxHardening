4d30f7a425de3783ccf027faf0ec3cc1975b41fa
FakeSmile <88681966+FakeSmileVX@users.noreply.github.com>
2022-02-20 01:02:41 +0300

    Create hardening.sh

---
+++ hardening.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# LinuxHardening v1.0
+#
+# FakeSmileVX and 0x733
+#
+# Github=> FakeSmileVX
+# Github=> 0x733
+#
+# Safer linux experience for advanced users...
+#
+# Websites
+#
+# www.fakesmile.art
+# www.inenarratus.com
+#
+# https://github.com/FakeSmileVX/LinuxHardening
+
+source shield.sh
+
+#################################################################################
+
+f_banner(){
+echo
+echo "
